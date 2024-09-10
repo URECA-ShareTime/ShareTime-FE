ac316f72df0f7d0a15a5a3fe1ddf46ce9426276f
OHRRRRR <3819713@naver.com>
2024-09-10 14:00:14 +0900

    feat - UI구현

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,15 @@
+import React from 'react';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
+import { BrowserRouter } from 'react-router-dom'; // BrowserRouter를 사용하여 전체 앱을 감싸기
 import './index.css';
 import Enterpage from './pages/Enterpage.jsx';
+import App from './App'; // App 컴포넌트 임포트
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Enterpage />
+    <BrowserRouter>
+      <App /> {/* App 컴포넌트를 BrowserRouter로 감싸서 렌더링 */}
+    </BrowserRouter>
   </StrictMode>
 );
