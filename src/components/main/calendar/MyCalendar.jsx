a57d836411eefc64c42e1b82e04b76fd9947e5eb
ongheong <pudding4271@gmail.com>
2024-09-24 17:26:07 +0900

    🐛 Fix: json 객체에서 state, end 문자열을 date 객체로 파싱하는 코드 추가

--- src/components/main/calendar/MyCalendar.jsx
+++ src/components/main/calendar/MyCalendar.jsx
@@ -7,10 +7,17 @@ import CustomToolbar from './CustomToolBar';
 import testEvents from '../../../mocks/testEvents.json';
 
 export default function MyCalendar() {
+  // JSON 데이터를 불러오고, 날짜를 Date 객체로 변환
+  const parsedEvents = testEvents.map(event => ({
+    ...event,
+    start: new Date(event.start),  // 문자열을 Date 객체로 변환
+    end: new Date(event.end)       // 문자열을 Date 객체로 변환
+  }));
   moment.locale('ko-KR');
-  const localizer = momentLocalizer(moment);
-  const [events, setEvents] = useState(testEvents);
 
+  const localizer = momentLocalizer(moment);
+  const [events, setEvents] = useState(parsedEvents);
+  
   return (
     <div className="w-full h-auto mx-[20px] my-[20px] mb-[30px] bg-primary">
       <Calendar
