9280fd67eef2347a6ab3264e24689c7b7683adce
ongheong <pudding4271@gmail.com>
2024-09-26 10:15:43 +0900

    ✨ Feat: 로고 클릭 시 MainPage 또는 EnterPage로 이동하는 기능 추가

--- src/components/common/header/Header.jsx
+++ src/components/common/header/Header.jsx
@@ -1,10 +1,16 @@
 import logoImg from '../../../assets/logo.png';
 import HeaderProfile from './HeaderProfile';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header({ img }) {
+  const navigate = useNavigate();
+  const handleClickLogo = () => {
+    navigate('/main');
+  }
+
   return (
     <div className="flex justify-between items-center bg-white h-header w-full px-[40px] py-[30px]">
-      <img src={logoImg} alt="logo" className="top-5 left-5 h-[30px]" />
+      <img src={logoImg} alt="logo" className="top-5 left-5 h-[30px]" onClick={handleClickLogo}/>
       <HeaderProfile />
     </div>
   );
