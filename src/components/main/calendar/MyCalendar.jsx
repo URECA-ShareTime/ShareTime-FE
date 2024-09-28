377b3104fdb041db3e4bf9b56126d0816a91297b
ongheong <pudding4271@gmail.com>
2024-09-28 12:42:20 +0900

    ✏️ Fix: MyCalendar에서 allevent get요청 연결

--- src/components/main/calendar/MyCalendar.jsx
+++ src/components/main/calendar/MyCalendar.jsx
@@ -1,55 +1,70 @@
 import { Calendar, momentLocalizer } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment';
-import { Button } from '@material-tailwind/react';
 import { useEffect, useState } from 'react';
 import CustomToolbar from './CustomToolBar';
-import testEvents from '../../../mocks/testEvents.json';
 import TaskModal from './TaskModal';
+import { getAllEvents } from '../../../api/event';
 
 export default function MyCalendar() {
-  // JSON 데이터를 불러오고, 날짜를 Date 객체로 변환
-  const parsedEvents = testEvents.map(event => ({
-    ...event,
-    start: new Date(event.start),  // 문자열을 Date 객체로 변환
-    end: new Date(event.end)       // 문자열을 Date 객체로 변환
-  }));
   moment.locale('ko-KR');
-
   const localizer = momentLocalizer(moment);
-  const [events, setEvents] = useState(parsedEvents);
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const getEvents = async () => {
+      const allEvents = await getAllEvents();
+      const parsedAllEvents = parsedEvents(allEvents);
+      setEvents(parsedAllEvents);
+    };
+    getEvents();
+  }, []);
+
+  // JSON 데이터를 불러오고, 날짜를 Date 객체로 변환
+  const parsedEvents = (originEvents) => {
+    return originEvents.map((event) => ({
+      ...event,
+      id: event.event_id,
+      classId: event.class_id ? [event.class_id] : [], // 값이 있을 때만 배열에 추가
+    studyId: event.study_id ? [event.study_id] : [], // 값이 있을 때만 배열에 추가
+    creator: event.creator_id, 
+    groupType: event.group_type ? event.group_type.split(',') : [], // 중첩 배열 제거
+      start: new Date(event.start_date), // 문자열을 Date 객체로 변환
+      end: new Date(event.end_date), // 문자열을 Date 객체로 변환
+    }));
+  };
 
   const [isModalOpen, setModalOpen] = useState(false);
   const [selectedDate, setSelectedDate] = useState(null);
   const [newEvent, setNewEvent] = useState({
     id: events.length + 1,
-    title: "",
-    description: "",
-    start: "",
-    end: "",
+    title: '',
+    description: '',
+    start: '',
+    end: '',
     groupType: [],
     classId: [],
     studyId: [],
-    creator: 1
+    creator: 1,
   });
   const [isEdit, setIsEdit] = useState(false);
 
-  const handleSelectSlot = ({start}) => {
+  const handleSelectSlot = ({ start }) => {
     setSelectedDate(start);
     setNewEvent((prevEvent) => ({
       ...prevEvent,
       start: start,
-      end: moment(start).add(1, 'hours').toDate()
+      end: moment(start).add(1, 'hours').toDate(),
     }));
     setModalOpen(true);
     setIsEdit(false);
   };
-  
+
   const handleSelectEvent = (event) => {
     setNewEvent(event);
     setModalOpen(true);
     setIsEdit(true);
-  }
+  };
 
   return (
     <div className="w-full h-auto mx-[20px] my-[20px] mb-[30px] bg-primary">
@@ -61,14 +76,16 @@ export default function MyCalendar() {
         defaultView="month" // 기본 화면은 월별
         views={['month', 'day']} // 월별, 일별로 변경 가능
         components={{
-          toolbar: (props) => <CustomToolbar {...props} setEvents={setEvents} />,
+          toolbar: (props) => (
+            <CustomToolbar {...props} setEvents={setEvents} />
+          ),
         }}
         className="bg-white text-gray-600"
         onSelectSlot={handleSelectSlot}
         onSelectEvent={handleSelectEvent}
         selectable
       />
-      <TaskModal 
+      <TaskModal
         isModalOpen={isModalOpen}
         setModalOpen={setModalOpen}
         newEvent={newEvent}
